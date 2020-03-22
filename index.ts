71583eefb81bd926d4d416b3270ccf3968863d66
hyeryung <s2019w38@e-mirim.hs.kr>
2020-03-22 14:04:09 +0900

    first

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-alert("hello");
+const name="cho",
+    age= 18,
+    gender ="fmale";
+
+const sayHi=(name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+sayHi(name,age);
+
+export {};
