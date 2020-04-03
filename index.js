858185868a1fbc86966a66332b1701cf0a1c1c57
hyeryung <s2019w38@e-mirim.hs.kr>
2020-04-03 21:24:35 +0900

    fnish

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "cho", age = 18, gender = "fmale";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
